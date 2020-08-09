44abc8355ec086707b373424ac2c8e6b05db35dd
Peiwen Lu <hi@peiwen.lu>
2020-08-09 21:52:07 +0800

    Upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -6,16 +6,15 @@ import { Minifier } from "css-loader-minify-class";
 const isProduction = !process.env.ROLLUP_WATCH;
 
 const minifier = new Minifier({ prefix: "_" }).getLocalIdent;
-const minifyClassname = localName => {
-  return minifier({ resourcePath: "" }, null, localName);
-};
+const minifyClass = (localName) => minifier({ resourcePath: "" }, null, localName);
 
-const generateStylesCljs = json => {
+const generateStylesCljs = (_, json) => {
   const lines = ["(ns app.styles)\n"];
   for (let [key, value] of Object.entries(json)) {
     lines.push(`(def ${key} "${value}")`);
   }
   writeFileSync("src/app/styles.cljs", lines.join("\n"));
+  console.log("=== Created styles.cljs ===");
 };
 
 export default [
@@ -33,13 +32,10 @@ export default [
         sourceMap: false,
         extensions: [".scss"],
         modules: {
-          generateScopedName: isProduction
-            ? minifyClassname
-            : "[local]-[hash:base64:5]",
-          getJSON: (_, json) => {
-            generateStylesCljs(json);
-          }
-        }
+          generateScopedName: isProduction ? minifyClass : "[local]-[hash:base64:5]",
+          getJSON: generateStylesCljs
+        },
+        autoModules: false
       })
     ]
   }
